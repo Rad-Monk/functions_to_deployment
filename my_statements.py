414bf22f2afacf78083d39c22c8f395a0cabd2b0
Aryan Sharma <114324726+Rad-Monk@users.noreply.github.com>
2024-06-27 12:29:44 +0000

    added basic files

---
+++ my_statements.py
@@ -0,0 +1,8 @@
+my_list = ["cheery","grapes","mango"]
+my_dict = { "fav hooman" : "my babyyyy" ,"fav thing to do" : "my babyyyy"}
+
+for item in my_list:
+    print(f"i love to eat {item}")
+
+for key, value in my_dict.items():
+    print(f"{key} is {value}")
